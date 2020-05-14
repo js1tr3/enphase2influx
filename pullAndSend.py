8fdad0d08bad948bfa40cc5b040753a8b7537d65
Jason Siegel <siegeljb@umich.edu>
2020-05-14 11:19:13 -0400

    update to python3

--- pullAndSend.py
+++ pullAndSend.py
@@ -9,7 +9,7 @@ __copyright__ = "Copyright 2018, FireMON, WaterMON, EarthMON, SpaceMON"
 __version__ = "1.1.0"
 
 import json
-import urllib2
+import urllib3
 from influxdb import InfluxDBClient
 import time
 import progressbar
@@ -18,16 +18,19 @@ import argparse
 # Sleep time setting
 __sleepTime__ = 30
 # InfluxDB settings
-__host__ = 'localhost'
+__host__ = "localhost"
 __port__ = 8086
-__user__ = 'admin'
-__password__ = 'admin'
-__dbname__ = 'enphase'
+__user__ = "admin"
+__password__ = "admin"
+__dbname__ = "enphase"
 
 # Getting arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--url', default="http://enphase.ayent/production.json",
-        help='the URL of production.json (default: http://enphase.ayent/production.json)')
+parser.add_argument(
+    "--url",
+    default="http://envoy.local/production.json",
+    help="the URL of production.json (default: http://envoy.local/production.json)",
+)
 
 args = parser.parse_args()
 __url__ = args.url
@@ -39,62 +42,67 @@ lastConsumptionTime = 0
 lastNetConsumptionTime = 0
 
 """Functions definitions"""
+
+
 def pushData(data, seriesName, client):
-        """Push data  into InfluxDB"""
-        valQuery = [1]
-        val = {}
-        val["fields"] = data
-        val["measurement"] = seriesName
-        valQuery[0] = val
-        client.write_points(valQuery)
+    """Push data  into InfluxDB"""
+    valQuery = [1]
+    val = {}
+    val["fields"] = data
+    val["measurement"] = seriesName
+    valQuery[0] = val
+    client.write_points(valQuery)
+
 
 client = InfluxDBClient(__host__, __port__, database=__dbname__)
 
 """Part 1 : Query production data from an url"""
-print "************************"
-print "Getting Enphase JSON information from server " + __url__
+print("************************")
+print("Getting Enphase JSON information from server " + __url__)
 # Query the url
 try:
-        response = urllib2.urlopen(__url__)
+    #response = urllib3.urlopen(__url__)
+    http = urllib3.PoolManager()
+    r = http.request('GET', __url__)
+    r.status
+    r.data
+    # Load response into json object
+    data = json.loads(response.read())
+    productionInverterData = data["production"][0]
+    productionEimData = data["production"][1]
+    consumptionData = data["consumption"][0]
+    netconsumptionData = data["consumption"][1]
 
-        # Load response into json object
-        data = json.loads(response.read())
-        productionInverterData = data['production'][0]
-        productionEimData = data['production'][1]
-        consumptionData = data['consumption'][0]
-        netconsumptionData = data['consumption'][1]
+    """Part 2 : Check if readingTime is different"""
+    print("************************")
+    print("Time data from general information : ", productionInverterData["readingTime"])
+    print("Time data from general data EIM : ", productionEimData["readingTime"])
+    print("Time data from consumption : ", consumptionData["readingTime"])
+    print("Time data from net consumption : ", netconsumptionData["readingTime"])
+    print("************************")
 
-        """Part 2 : Check if readingTime is different"""
-        print "************************"
-        print "Time data from general information : ", productionInverterData['readingTime']
-        print "Time data from general data EIM : ", productionEimData['readingTime']
-        print "Time data from consumption : ", consumptionData['readingTime']
-        print "Time data from net consumption : ", netconsumptionData['readingTime']
-        print "************************"
+    """Part 3 : Push data into InfluxDB, only if time is different"""
+    if productionInverterData["readingTime"] > lastProductionInverterTime:
+        print("Pushing production data")
+        pushData(productionInverterData, "general_info", client)
+        lastProductionInverterTime = productionInverterData["readingTime"]
 
-        """Part 3 : Push data into InfluxDB, only if time is different"""
-        if productionInverterData['readingTime'] > lastProductionInverterTime:
-                print "Pushing production data"
-                pushData(productionInverterData, "general_info", client)
-                lastProductionInverterTime = productionInverterData['readingTime']
+    if productionEimData["readingTime"] > lastProductionEimTime:
+        print("Pushing general info")
+        pushData(productionEimData, "production", client)
+        lastProductionEimTime = productionEimData["readingTime"]
 
-        if productionEimData['readingTime'] > lastProductionEimTime:
-                print "Pushing general info"
-                pushData(productionEimData, "production", client)
-                lastProductionEimTime = productionEimData['readingTime']
+    if consumptionData["readingTime"] > lastConsumptionTime:
+        print("Pushing total consumption data")
+        pushData(consumptionData, "total_consumption", client)
+        lastConsumptionTime = consumptionData["readingTime"]
 
-        if consumptionData['readingTime'] > lastConsumptionTime:
-                print "Pushing total consumption data"
-                pushData(consumptionData, "total_consumption", client)
-		lastConsumptionTime = consumptionData['readingTime']
-
-        if netconsumptionData['readingTime'] > lastNetConsumptionTime:
-                print "Pushing net consumption"
-                pushData(netconsumptionData, "net_consumption", client)
-                lastNetConsumptionTime = netconsumptionData['readingTime']
+    if netconsumptionData["readingTime"] > lastNetConsumptionTime:
+        print("Pushing net consumption")
+        pushData(netconsumptionData, "net_consumption", client)
+        lastNetConsumptionTime = netconsumptionData["readingTime"]
 
 except Exception as e:
-        print e
+    print(e)
 
-print "************************"
-
+print("************************")
